4c372ef3c186cac9508246038119d2802caa82eb
AlexPanainte <panainte_alex@yahoo.com>
2024-01-04 13:23:47 +0200

    cart Icon update functionality

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -5,59 +5,72 @@ import json
 
 
 def store(request):
-	products=Product.objects.all()
-	context = {'products': products}
-	return render(request, 'store/store.html', context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items  # Corrected this line
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
+
+    products = Product.objects.all()
+    context = {'products': products, 'cartItems': cartItems}
+    return render(request, 'store/store.html', context)
+
 
 def cart(request):
-	if request.user.is_authenticated:
-		customer=request.user.customer
-		order, created= Order.objects.get_or_create(customer=customer, complete=False)
-		items=order.orderitem_set.all()
-	else:
-		items=[]
-		order={'get_cart_total':0,'get_cart_items':0}
-	
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
 
-	context = {'order': order, 'items': items}
-	return render(request, 'store/cart.html', context)
+    context = {'order': order, 'items': items, 'cartItems':cartItems }
+    return render(request, 'store/cart.html', context)
+
 
 def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
 
-	if request.user.is_authenticated:
-		customer=request.user.customer
-		order, created= Order.objects.get_or_create(customer=customer, complete=False)
-		items=order.orderitem_set.all()
-	else:
-		items=[]
-		order={'get_cart_total':0,'get_cart_items':0}
-	
+    context = {'order': order, 'items': items, 'cartItems':cartItems }
+    return render(request, 'store/checkout.html', context)
 
-	context = {'order': order, 'items': items}
-	return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
-	data= json.loads(request.body)
-	productId= data['productId']
-	action=data['action']
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
 
-	print("Action",action)
-	print("productId", productId)
+    print("Action", action)
+    print("productId", productId)
 
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-	customer = request.user.customer
-	product = Product.objects.get(id=productId)
-	order, created= Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
-	orderItem, created = OrderItem.objects.get_or_create(order=order,product=product)
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    orderItem.save()
 
-	if action =='add':
-		orderItem.quantity = (orderItem.quantity+1)
-	elif action=='remove':
-		orderItem.quantity = (orderItem.quantity-1)
-	orderItem.save()
+    if orderItem.quantity <= 0:
+        orderItem.delete()
 
-	if orderItem.quantity <=0:
-		orderItem.delete()
-
-	return JsonResponse("Item was added", safe=False)
+    return JsonResponse({'message': "Item was added"}, safe=False)  
